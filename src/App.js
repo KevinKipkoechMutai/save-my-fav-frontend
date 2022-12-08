c89edc6bd4290eea821ab71f2236dfd7868c0315
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-12-08 10:21:21 +0300

    frontend

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import Navbar from './Navbar'
+import HeroSection from './HeroSection'
+import MyFavorites from './MyFavorites'
+import Footer from './Footer'
+
+function App() {
+  
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <MyFavorites />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
